3371dc8e92cdf825e2f68503808055f5e9cfa9c5
Frankie L <67761168+frankjlin16@users.noreply.github.com>
2024-09-17 02:04:58 -0700

    Initial commit

---
+++ lua/config/options.lua
@@ -0,0 +1,39 @@
+vim.cmd("let g:netrw_liststyle = 3")
+
+local opt = vim.opt
+
+opt.relativenumber = true
+opt.number = true
+
+-- tabs and indentation
+opt.tabstop = 2 -- 2 spaces for tabs
+opt.shiftwidth = 2 -- 2 spaces for indent width
+opt.expandtab = true -- expand tab to spaces
+opt.autoindent = true -- copy indent from current line when starting new one
+
+opt.wrap = false
+
+-- searchs settings
+opt.ignorecase = true -- ignore case when searching
+opt.smartcase = true -- if you include mixed case in search, assume you want case-sensitive
+
+opt.cursorline = true
+
+-- turn on termguicolors for colorscheme to work
+opt.termguicolors = true
+opt.background = "dark" -- colorschemes that can be light or dark will be made dark
+opt.signcolumn = "yes" -- show sign column so that text doesn't shift
+
+-- backspace
+opt.backspace = "indent,eol,start" -- allow backspace on indent, end of line or insert mode start position
+
+-- clipboard
+opt.clipboard:append("unnamedplus") -- use system clipboard as default register
+
+-- split windows
+opt.splitright = true
+opt.splitbelow = true
+
+-- turn off swapfile
+opt.swapfile = false
+
